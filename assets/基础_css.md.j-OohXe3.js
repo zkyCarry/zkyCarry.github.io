6334e785e5ab17bdfbb4e38e2f12f6175768cc68
zhang.kaiyu <zhang.kaiyu@immomo.com>
2024-09-26 12:34:21 +0800

    add

--- assets/基础_css.md.j-OohXe3.js
+++ assets/基础_css.md.j-OohXe3.js
@@ -10,7 +10,7 @@ import{_ as s,c as a,o as i,a1 as n}from"./chunks/framework.D01xxF7t.js";const g
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">3.</span></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">继承：文字和表格类的。 </span></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">主动继承 属性：inherit；</span></span>
-<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">4. rem root节点下字体大小 font-size:16px;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">4. rem root节点下字体大小 font-size:16px;  em是父节点的font-size</span></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">:root {</span></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">	font-s</span></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">}</span></span>
@@ -48,7 +48,8 @@ import{_ as s,c as a,o as i,a1 as n}from"./chunks/framework.D01xxF7t.js";const g
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">15. flex 布局</span></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">父亲， display：flex； 子集 :last-child{margin-left:auto} 最后一额高  auto代表自动适应，会把父级填满。 margin： 0 auto； 剧中。</span></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">子类： flex 缩写： flex-grow 比例  flex-shrink(可缩放) flex-basis。默认： 1，1 0；  flex-grow：2, 两倍 其他一倍， flex-shrink 是否缩放 0 不缩放。  self-algin。</span></span>
-<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">justify-content 主轴对齐， align-items 横轴对齐。 flex-direction： 设置主轴横轴。   flex-wrap：no-warp； wrap-revese。 </span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">justify-content 主轴对齐， align-items 横轴对齐。  flex-direction： 设置主轴横轴。   flex-wrap：no-warp； wrap-revese。 </span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">align-content: stretch；针对多行的横轴对齐。 默认 stretch：拉伸。 align-content: flex-start;就会去掉被拉伸的情况，(这里做监控墙的时候有被坑过)</span></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">子类：flex-order 数值越小越靠前。</span></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">flex:1; 是 flex：1 1 0; 简写。如果其他的都有设置了with， flex:1 这个元素会会自动填充剩下的空间。(自适应)</span></span>
 <span class="line"></span>
@@ -73,7 +74,7 @@ import{_ as s,c as a,o as i,a1 as n}from"./chunks/framework.D01xxF7t.js";const g
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">22. 盒模型： margin border padding width hight</span></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">23. 响应式设计： 移动端有限， @media 媒体查询， 设置meta  width：device-width， initial-scale=1； user-scalabel=no； &lt;</span><span style="--shiki-light:#22863A;--shiki-dark:#85E89D;">meta</span><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;"> name</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">=</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;viewport&quot;</span><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;"> content</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">=</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;width:750, user-scalable=no&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> &gt; 陌陌。</span></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">24. img标签， 不同的分辨率可以使用不同过得图片 img srcset; img{max-width: 100%}</span></span>
-<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">25. reset.css 有库。 normalize.css</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">25. reset.css 有库。 normalize.css， 去掉浏览器默认的一些格式。</span></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">26. bem 命名 -连字符  __双下划线子元素 一种状态 双横线</span></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">27. css模块，写的时候分开，打包的时候合在一起。</span></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">28. 整理自己的css框架，别完全使用别人的。</span></span>
@@ -90,4 +91,4 @@ import{_ as s,c as a,o as i,a1 as n}from"./chunks/framework.D01xxF7t.js";const g
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">35. scss @mixin @include @extend 继承。 @for @if。.xx{&amp;。xxx 控制当前节点有效}。 .xx {.xxx 控制子节点的}  </span></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">36。 近大远小。 设置透视，perspective 子类可以共享父亲级别。 rotateX rotateY。 </span></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">37. 优先级 ！import  行内样式 id选择器， 类选择器， 通配符选择器。 伪类优先级和类选择器一样。</span></span>
-<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">38. 主要应用在自适应的网页：双飞翼：三栏布局，两边固定宽度，中间内容自适应宽度。 圣杯：包含页头和页脚。中间是双飞翼。</span></span></code></pre></div>`,4),t=[p];function e(E,h,k,r,d,c){return i(),a("div",null,t)}const y=s(l,[["render",e]]);export{g as __pageData,y as default};
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">38. 主要应用在自适应的网页：双飞翼：三栏布局，两边固定宽度，中间内容自适应宽度。 圣杯：包含页头和页脚。中间是双飞翼。</span></span></code></pre></div>`,4),t=[p];function e(E,h,k,r,c,d){return i(),a("div",null,t)}const y=s(l,[["render",e]]);export{g as __pageData,y as default};
